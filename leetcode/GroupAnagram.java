318b967662fa62813f80270973e6af052f50f01d
Allwin <allwin2436@gmail.com>
2026-02-25 13:45:10 +0530

    MEDIUM: 49. Group Anagram

---
+++ leetcode/GroupAnagram.java
@@ -0,0 +1,18 @@
+class GroupAnagram {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap <String, List<String>> map= new HashMap<>();
+        for (String str: strs){
+             char [] temp = str.toCharArray();
+             Arrays.sort(temp);
+            String sorted = new String (temp);
+            if (!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+            
+             map.get(sorted).add(str);
+            
+        }
+        return new ArrayList<>(map.values());
+    }
+}
