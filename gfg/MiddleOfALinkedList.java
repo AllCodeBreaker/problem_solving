224e738216c23c234566465d319c2950121d364e
Allwin <allwin2436@gmail.com>
2026-07-17 21:09:16 +0530

    Easy: Middle Of A Linked List

---
+++ gfg/MiddleOfALinkedList.java
@@ -0,0 +1,25 @@
+//Middle of a Linked List
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+} */
+
+class MiddleOfALinkedList {
+    int getMiddle(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+}
