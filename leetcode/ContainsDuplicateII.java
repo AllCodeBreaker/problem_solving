e642eb7d549b405235f20b367e26a05d190f3e67
Allwin <allwin2436@gmail.com>
2026-08-01 14:56:38 +0530

    Easy: Contains Duplicate II

---
+++ leetcode/ContainsDuplicateII.java
@@ -0,0 +1,19 @@
+//219. Contains Duplicate II
+class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++ ){
+            if(map.containsKey(nums[i])){
+                if (Math.abs(i-map.get(nums[i])) <= k ){
+                    return true;
+                }
+            }
+
+            map.put(nums[i],i);
+            
+        }
+        return false;
+    }
+}
+//Time O(n)
+//Space O(n)
